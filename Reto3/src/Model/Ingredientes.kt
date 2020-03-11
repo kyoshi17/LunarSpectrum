8b221aea70335d4b2d29bb0262bae141703d6a8a
kyoshi17 <61947045+kyoshi17@users.noreply.github.com>
2020-03-11 19:16:29 -0300

    Reto3

---
+++ Reto3/src/Model/Ingredientes.kt
@@ -0,0 +1,17 @@
+package Model
+
+class Ingredientes () {
+    val ingredientes: String
+        get() = """" 
+                Selecciona por categoria el ingrediente que buscas:
+                1. Agua
+                2. Lacteos
+                3. Carnes
+                4. Verduras
+                5. Frutas
+                6. Cereales
+                7.Huevos
+                8.Legunbres
+                9.Aceites"""
+
+}
